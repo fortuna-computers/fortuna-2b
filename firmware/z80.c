5b59d973bc25971c12bed5776bf53b807e064b33
André Wagner <WagnerAndre@JohnDeere.com>
2021-11-23 13:00:29 -0300

    .

--- firmware/z80.c
+++ firmware/z80.c
@@ -3,8 +3,6 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-volatile uint16_t z80_speed_khz = 0;
-
 #define set_BUSRQ()    PORTB |=  (1 << PB0)
 #define clear_BUSRQ()  PORTB &= ~(1 << PB0)
 #define set_RST()      PORTB |=  (1 << PB2)
@@ -21,23 +19,8 @@ volatile uint16_t z80_speed_khz = 0;
 #define get_BUSACK()   (PINB & (1 << PINB1))
 #define get_IORQ()     (PIND & (1 << PIND3))
 
-static void z80_powerdown(void)
-{
-    // reset (initialize as low)
-    clear_RST();
-    
-    // run 50 cycles
-    TCCR1A = 0;
-    for (uint8_t i = 0; i < 50; ++i) {
-        set_CLK();
-        clear_CLK();
-    }
-}
-
 void z80_init(uint16_t speed_khz)
 {
-    z80_speed_khz = speed_khz;
-    
     // output: BUSRQ, RST, WAITST
     DDRB |= _BV(DDB0) | _BV(DDB2) | _BV(DDB3);
     
@@ -61,7 +44,7 @@ void z80_init(uint16_t speed_khz)
     set_NMI();
     
     // clock output on OC1A (PD5)
-    OCR1A = F_CPU / (2UL * (unsigned long) z80_speed_khz * 1000UL) - 1UL;  // calculate speed
+    OCR1A = F_CPU / (2UL * (unsigned long) speed_khz * 1000UL) - 1UL;  // calculate speed
     TCCR1A = (1 << COM1A0);                  // Toggle OC1A/OC1B on compare match
     TCCR1B = (1 << WGM12) | (1 << CS10);     // CTC mode, top OCR1A, no prescaling (clk/1)
     
