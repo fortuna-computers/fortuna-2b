6604f7113f29f813e86a60b14332975662917485
André Wagner <WagnerAndre@JohnDeere.com>
2021-11-25 20:21:56 +0000

    .

--- firmware/addr.c
+++ firmware/addr.c
@@ -1,59 +1,71 @@
 #include "addr.h"
 
 #include <avr/io.h>
+#include <util/delay.h>
 
-// pins used
-#define SER_H PC2
-#define SER_L PC3
-#define SRCLK PC4
-#define OE    PC5
+#include "uart.h"
+
+#define set_SER_H()   PORTC |=  (1 << PC2)
+#define clear_SER_H() PORTC &= ~(1 << PC2)
+#define set_SER_L()   PORTC |=  (1 << PC3)
+#define clear_SER_L() PORTC &= ~(1 << PC3)
+#define set_SRCLK()   PORTC |=  (1 << PC4)
+#define clear_SRCLK() PORTC &= ~(1 << PC4)
+#define set_OE()      PORTC |=  (1 << PC5)
+#define clear_OE()    PORTC &= ~(1 << PC5)
+#define set_RCLK()    PORTD |=  (1 << PD7)
+#define clear_RCLK()  PORTD &= ~(1 << PD7)
 
 void addr_init(void)
 {
     DDRC |= _BV(DDC2) | _BV(DDC3) | _BV(DDC4) | _BV(DDC5);
-    PORTC |= _BV(OE);
+    DDRD |= _BV(DDD7);
+    set_OE();
 
+    clear_SER_L();
+    clear_SER_H();
     for (int i = 0; i < 9; ++i) {   // clear 595
-        PORTC |= _BV(SRCLK);
-        PORTC &= ~_BV(SRCLK);
+        clear_SRCLK();
+        set_SRCLK();
     }
+    clear_RCLK();
+    set_RCLK();
 }
 
 void addr_set(uint16_t addr)
 {
-    // disable 595
-    PORTC |= _BV(OE);
+    // disable 595 output
+    set_OE();
 
-    uint8_t portc = PORTC & 0b11100011;  // bytes to keep
+    set_SRCLK();
+    clear_SRCLK();
 
+    // additional clock cycle
     for (int i = 0; i < 8; ++i) {
 
-        // set SER_L
+        // set low ADDR
         if ((addr >> i) & 1)
-            portc |= _BV(SER_L);
+            set_SER_L();
         else
-            portc &= ~_BV(SER_L);
+            clear_SER_L();
 
-        // set SER_H
         if ((addr >> (i + 8)) & 1)
-            portc |= _BV(SER_H);
+            set_SER_H();
         else
-            portc &= ~_BV(SER_H);
-        
-        // 595 cycle
-        PORTC = portc;
-        PORTC |= _BV(SRCLK);
+            clear_SER_H();
+
+        // cycle
+        set_SRCLK();
+        clear_SRCLK();
     }
 
-    // extra CYCLE because RCLK and SRCLK are connected, so RCLK is one cycle behind
-    PORTC &= ~_BV(SRCLK);
-    PORTC |= _BV(SRCLK);
-
-    // enable 595
-    PORTC &= ~_BV(OE);
+    // enable 595 output
+    clear_RCLK();
+    set_RCLK();
+    clear_OE();
 }
 
 void addr_disable(void)
 {
-    PORTC |= _BV(OE);
+    set_OE();
 }
